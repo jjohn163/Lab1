e88515c360fcc11f7fd1d02b6cbf437007a7ed5f
jjohn163 <jjohn163@calpoly.edu>
2020-04-19 11:28:10 -0700

    cleaned code of unnecessary variables/functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,3 @@
-/*
- * Program 3 base code - includes modifications to shape and initGeom in preparation to load
- * multi shape objects 
- * CPE 471 Cal Poly Z. Wood + S. Sueda + I. Dunn
- */
 
 #include <iostream>
 #include <glad/glad.h>
@@ -37,20 +32,12 @@ public:
 	GLFWwindow *window = nullptr;
 
 	// Our shader program
-	std::shared_ptr<Program> prog;
 	std::shared_ptr<Program> progMat;
-	std::shared_ptr<Program> progDogs;
 	std::shared_ptr<Program> cubeProg;
 	// Shape to be used (from  file) - modify to support multiple
-	shared_ptr<Shape> mesh;
 	shared_ptr<Shape> meshfloor;
 	shared_ptr<Shape> meshsphere;
-	shared_ptr<Shape> meshdog2;
-	shared_ptr<Shape> meshtree;
-	shared_ptr<Shape> meshpeng;
-	shared_ptr<Texture> texture0;
-	shared_ptr<Texture> texture1;
-	shared_ptr<Texture> texture2;
+
 
 	// Contains vertex information for OpenGL
 	GLuint VertexArrayID;
@@ -70,23 +57,17 @@ public:
 	bool movingBackward = false;
 	bool movingLeft = false;
 	bool movingRight = false;
-	string specifiedOBJ = "/bunny.obj";
-	vector<vec3> head;
-	vector<vec3> body;
-	vector<vec3> bottom;
-	vector<vec3> positions;
+
 	vector<vec3> treePositions;
-	vector<vec3> pengPositions;
-	vector<shared_ptr<Shape>> multShape;
+
 	float phi = 0;
 	float pheta = 1.5708;
 	vec3 lookAtPoint = vec3(0, 0, 1);
 	vec3 eye = vec3(0, 0, 0);
 	vec3 up = vec3(0, 1, 0);
-	int matSelect = 0;
-	float shape_scale;
-	float speed = .5;
-	float SPHERE_SIZE = .025;
+
+	float PLAYER_SPEED = 10.0;
+	float deltaTime = 0;
 	float PI = 3.14159;
 	float rotate = 0;
 	vec3 light = vec3(-2, 2, 2);
@@ -101,9 +82,6 @@ public:
 		"iceflow_bk.tga"
 	};
 
-	vec3 crossProduct(vec3 u, vec3 v) {
-		return vec3((u.y*v.z - u.z*v.y), (u.z*v.x - u.x*v.z), (u.x*v.y - u.y*v.x));
-	}
 
 	void updateLookAtPoint() {
 		if (phi > 1.4) {
@@ -131,7 +109,7 @@ public:
 		if (movingForward) {
 			vec3 direction = lookAtPoint - eye;
 			vec3 w = normalize(direction);
-			vec3 delta = speed * w;
+			vec3 delta = PLAYER_SPEED * deltaTime * w;
 			delta.y = 0;
 			eye += delta;
 			lookAtPoint += delta;
@@ -139,7 +117,7 @@ public:
 		if (movingBackward) {
 			vec3 direction = lookAtPoint - eye;
 			vec3 w = normalize(direction);
-			vec3 delta = speed * w;
+			vec3 delta = PLAYER_SPEED * deltaTime * w;
 			delta.y = 0;
 			eye -= delta;
 			lookAtPoint -= delta;
@@ -148,15 +126,15 @@ public:
 			vec3 direction = lookAtPoint - eye;
 			vec3 w = normalize(direction);
 			vec3 u = normalize(cross(up, w));
-			eye += speed * u;
-			lookAtPoint += speed * u;
+			eye += PLAYER_SPEED * deltaTime * u;
+			lookAtPoint += PLAYER_SPEED * deltaTime * u;
 		}
 		if (movingRight) {
 			vec3 direction = lookAtPoint - eye;
 			vec3 w = normalize(direction);
 			vec3 u = normalize(cross(up, w));
-			eye -= speed * u;
-			lookAtPoint -= speed * u;
+			eye -= PLAYER_SPEED * deltaTime * u;
+			lookAtPoint -= PLAYER_SPEED * deltaTime * u;
 		}
 	}
 
@@ -196,9 +174,6 @@ public:
 		if (key == GLFW_KEY_E && action == GLFW_PRESS) {
 			light.x += 0.5;
 		}
-		if (key == GLFW_KEY_M && action == GLFW_PRESS) {
-			matSelect = (matSelect + 1) % 4;
-		}
 		if (key == GLFW_KEY_Z && action == GLFW_PRESS) {
 			glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
 		}
@@ -206,7 +181,7 @@ public:
 			glPolygonMode( GL_FRONT_AND_BACK, GL_FILL );
 		}
 	}
-
+	
 	void mouseCallback(GLFWwindow *window, int button, int action, int mods)
 	{
 		double posX, posY;
@@ -255,21 +230,6 @@ public:
 
 	// Code to load in the three textures
 	void initTex(const std::string& resourceDirectory) {
-		texture0 = make_shared<Texture>();
-		texture0->setFilename(resourceDirectory + "/cheetah.jpg");
-		texture0->init();
-		texture0->setUnit(0);
-		texture0->setWrapModes(GL_CLAMP_TO_EDGE, GL_CLAMP_TO_EDGE);
-		texture1 = make_shared<Texture>();
-		texture1->setFilename(resourceDirectory + "/tiger.jpg");
-		texture1->init();
-		texture1->setUnit(1);
-		texture1->setWrapModes(GL_CLAMP_TO_EDGE, GL_CLAMP_TO_EDGE);
-		texture2 = make_shared<Texture>();
-		texture2->setFilename(resourceDirectory + "/grass.jpg");
-		texture2->init();
-		texture2->setUnit(2);
-		texture2->setWrapModes(GL_CLAMP_TO_EDGE, GL_CLAMP_TO_EDGE);
 		std::string cracksDirectory = resourceDirectory + "/sb_iceflow/";
 		skyTextureId = createSky(cracksDirectory, faces);
 	}
@@ -285,23 +245,6 @@ public:
 		glEnable(GL_DEPTH_TEST);
 		initTex(resourceDirectory);
 		// Initialize the GLSL program.
-		prog = make_shared<Program>();
-		prog->setVerbose(true);
-		prog->setShaderNames(resourceDirectory + "/tex_vert.glsl", resourceDirectory + "/tex_frag0.glsl");
-		prog->init();
-		prog->addUniform("P");
-		prog->addUniform("V");
-		prog->addUniform("M");
-		//prog->addUniform("MatDif");
-		//prog->addUniform("MatAmb");
-		//prog->addUniform("MatSpec");
-		prog->addUniform("Texture0");
-		//prog->addUniform("shine");
-		prog->addUniform("LightPos");
-		prog->addAttribute("vertPos");
-		prog->addAttribute("vertNor");
-		prog->addAttribute("vertTex");
-
 
 		cubeProg = make_shared<Program>();
 		cubeProg->setVerbose(true);
@@ -334,17 +277,7 @@ public:
 		window = windowManager->getHandle();
 
 	}
-	void pushToVector(vec3 newVect, int object) {
-		if (object == 0) {
-			head.push_back(newVect);
-		}
-		else if(object == 1){
-			body.push_back(newVect);
-		}
-		else {
-			bottom.push_back(newVect);
-		}
-	}
+
 
 	void initGeom(const std::string& resourceDirectory)
 	{
@@ -353,68 +286,14 @@ public:
 		// Initialize mesh
 		// Load geometry
  		// Some obj files contain material information.We'll ignore them for this assignment.
- 		vector<tinyobj::shape_t> TOshapes;
- 		vector<tinyobj::material_t> objMaterials;
  		string errStr;
 		vector<tinyobj::shape_t> TOshapesFloor;
 		vector<tinyobj::material_t> objMaterialsFloor;
 		vector<tinyobj::shape_t> TOshapesSphere;
 		vector<tinyobj::material_t> objMaterialsSphere;
-		vector<tinyobj::shape_t> TOshapesMult;
-		vector<tinyobj::material_t> objMaterialsMult;
-		vector<tinyobj::shape_t> TOshapesDog2;
-		vector<tinyobj::material_t> objMaterialsDog2;
-		vector<tinyobj::shape_t> TOshapesTree;
-		vector<tinyobj::material_t> objMaterialsTree;
-		vector<tinyobj::shape_t> TOshapesPeng;
-		vector<tinyobj::material_t> objMaterialsPeng;
 
-		//load in the mesh and make the shape(s)
-		//mesh for the simple spheres that make up the snowmen
- 		bool rc = tinyobj::LoadObj(TOshapes, objMaterials, errStr, (resourceDirectory + "/dog.obj").c_str());
-		if (!rc) {
-			cerr << errStr << endl;
-		} else {
-			mesh = make_shared<Shape>();
-			mesh->createShape(TOshapes[0]);
-			mesh->measure();
-			mesh->init();
-		}
-		//mesh for the second dog
-		rc = tinyobj::LoadObj(TOshapesDog2, objMaterialsDog2, errStr, (resourceDirectory + "/dog2.obj").c_str());
-		if (!rc) {
-			cerr << errStr << endl;
-		}
-		else {
-			meshdog2 = make_shared<Shape>();
-			meshdog2->createShape(TOshapesDog2[0]);
-			meshdog2->measure();
-			meshdog2->init();
-		}
 
-		rc = tinyobj::LoadObj(TOshapesTree, objMaterialsTree, errStr, (resourceDirectory + "/tree.obj").c_str());
-		if (!rc) {
-			cerr << errStr << endl;
-		}
-		else {
-			meshtree = make_shared<Shape>();
-			meshtree->createShape(TOshapesTree[0]);
-			meshtree->measure();
-			meshtree->init();
-		}
-
-		rc = tinyobj::LoadObj(TOshapesPeng, objMaterialsPeng, errStr, (resourceDirectory + "/penguin.obj").c_str());
-		if (!rc) {
-			cerr << errStr << endl;
-		}
-		else {
-			meshpeng = make_shared<Shape>();
-			meshpeng->createShape(TOshapesPeng[0]);
-			meshpeng->measure();
-			meshpeng->init();
-		}
-
-		rc = tinyobj::LoadObj(TOshapesFloor, objMaterialsFloor, errStr, (resourceDirectory + "/cube.obj").c_str());
+		bool rc = tinyobj::LoadObj(TOshapesFloor, objMaterialsFloor, errStr, (resourceDirectory + "/cube.obj").c_str());
 		if (!rc) {
 			cerr << errStr << endl;
 		}
@@ -436,39 +315,6 @@ public:
 			meshsphere->init();
 		}
 
-		rc = tinyobj::LoadObj(TOshapesMult, objMaterialsMult, errStr, (resourceDirectory + specifiedOBJ).c_str());
-		if (!rc) {
-			cerr << errStr << endl;
-		}
-		else {
-			float miny = 100000;
-			float maxy = -100000;
-			for (int i = 0; i < TOshapesMult.size(); i++) {
-				shared_ptr<Shape> part = make_shared<Shape>();
-				part->createShape(TOshapesMult[i]);
-				part->measure();
-				part->init();
-				multShape.push_back(part);
-				if (miny > part->min.y) {
-					miny = part->min.y;
-				}
-				if (maxy < part->max.y) {
-					maxy = part->max.y;
-				}
-			}
-			
-			shape_scale = 3 / (maxy - miny);
-			//cout << "scale = " << shape_scale << endl;
-		}
-		
-		//read out information stored in the shape about its size - something like this...
-		//then do something with that information.....
-		gMin.x = mesh->min.x;
-		gMin.y = mesh->min.y;
-		
-
-
-
 		treePositions.push_back(vec3(8, 0, 0));
 		treePositions.push_back(vec3(-8, 0, 0));
 		treePositions.push_back(vec3(0, 0, 8));
@@ -482,9 +328,6 @@ public:
 		treePositions.push_back(vec3(7, 0, 4));
 		treePositions.push_back(vec3(4, 0, 7));
 
-
-		pengPositions.push_back(vec3(-3, -1, 3));
-		pengPositions.push_back(vec3(-2.2, -1, 3.7));
 	}
 
 	void setModel(std::shared_ptr<Program> prog, std::shared_ptr<MatrixStack>M) {
@@ -538,57 +381,13 @@ public:
 		}
 	}
 
-
-
-	void drawDogs(shared_ptr<MatrixStack> Model) {
-		Model->pushMatrix();
-		Model->rotate(1, vec3(0, 1, 0));
-		Model->translate(vec3(2.3, -2, 4));
-		Model->scale(vec3(.06, .06, .06));
-		Model->rotate(-2.9, vec3(0, 1, 0));
-		Model->rotate(-1.5, vec3(1, 0, 0));
-		Model->translate(vec3(5.777, 16.61, 0));
-		setModel(prog, Model);
-		texture0->bind(prog->getUniform("Texture0"));
-		mesh->draw(prog);
-		Model->popMatrix();
-
-		Model->pushMatrix();
-		Model->rotate(1, vec3(0, 1, 0));
-		Model->translate(vec3(0, -2.2, -4));
-		Model->rotate(-1.5, vec3(1, 0, 0));
-		Model->scale(vec3(.04, .04, .04));
-		Model->translate(vec3(5.777, 16.01, 0));
-		setModel(prog, Model);
-		texture1->bind(prog->getUniform("Texture0"));
-		meshdog2->draw(prog);
-		Model->popMatrix();
-	}
-
-	void drawPenguins(shared_ptr<MatrixStack> Model) {
-		int mat = 5;
-		for (vec3 pos : pengPositions) {
-			SetMaterial(mat);
-			Model->pushMatrix();
-				Model->translate(pos);
-				Model->scale(vec3(.15, .15, .15));
-				Model->rotate(0.8, vec3(0, 1, 0));
-				Model->rotate(-1.57, vec3(1, 0, 0));
-				Model->translate(vec3(-3.05, 0.38, -0.28));
-				setModel(progMat, Model);
-				meshpeng->draw(progMat);
-			Model->popMatrix();
-			mat++;
-		}
-		
-	}
-	void drawTrees(shared_ptr<MatrixStack> Model) {
+	void drawEnemies(shared_ptr<MatrixStack> Model) {
 		SetMaterial(3);
 		for (vec3 pos : treePositions) {
 			Model->pushMatrix();
 			Model->translate(pos);
 			setModel(progMat, Model);
-			meshtree->draw(progMat);
+			meshsphere->draw(progMat);
 			Model->popMatrix();
 		}
 
@@ -605,8 +404,8 @@ public:
 	}
 	void render() {
 		TimeManager::Instance()->Update();
-		cout << TimeManager::Instance()->FrameRate() << endl;
-		cout << TimeManager::Instance()->DeltaTime() << endl;
+		deltaTime = TimeManager::Instance()->DeltaTime();
+		//cout << TimeManager::Instance()->FrameRate() << endl;
 		// Get current frame buffer size.
 		int width, height;
 		
@@ -633,17 +432,7 @@ public:
 		Projection->perspective(45.0f, aspect, 0.01f, 100.0f);
 
 		// Draw a stack of cubes with indiviudal transforms
-		prog->bind();
-		glUniformMatrix4fv(prog->getUniform("P"), 1, GL_FALSE, value_ptr(Projection->topMatrix()));
-		glUniformMatrix4fv(prog->getUniform("V"), 1, GL_FALSE, value_ptr(View));
-		glUniform3f(prog->getUniform("LightPos"), light.x, light.y, light.z);
 		
-			Model->pushMatrix();
-				Model->loadIdentity();
-				Model->rotate(rotate, vec3(0, 1, 0));
-				drawDogs(Model);
-			Model->popMatrix();
-		prog->unbind();
 		
 		progMat->bind();
 			glUniformMatrix4fv(progMat->getUniform("P"), 1, GL_FALSE, value_ptr(Projection->topMatrix()));
@@ -653,8 +442,7 @@ public:
 			Model->loadIdentity();
 				Model->rotate(rotate, vec3(0, 1, 0));
 				drawFloor(Model);
-				drawTrees(Model);
-				drawPenguins(Model);
+				drawEnemies(Model);
 			Model->popMatrix();
 		progMat->unbind();
 
@@ -708,11 +496,7 @@ int main(int argc, char *argv[])
 	windowManager->init(640, 480);
 	windowManager->setEventCallbacks(application);
 	application->windowManager = windowManager;
-	if (argc >= 2)
-	{
-		std::string name (argv[1]);
-		application->specifiedOBJ = "/" + name;
-	}
+	
 
 	// This is the code that will likely change program to program as you
 	// may need to initialize or set up different data and state
