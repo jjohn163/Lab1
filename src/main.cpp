36a2ee0b66f473243684ab613ef4fe0cd0b703f3
Kevin Krein <kkrein@calpoly.edu>
2020-05-03 23:23:31 -0700

    Made generation a lot different, added plane

--- src/main.cpp
+++ src/main.cpp
@@ -94,7 +94,7 @@ public:
 	float deltaTime = 0;
 	float PI = 3.14159;
 	float rotate = 0;
-	vec3 light = vec3(-60, 60, 2);
+	vec3 light = vec3(2000, 2000, 2000);
 	unsigned int skyTextureId;
 
 	float SPHERE_RADIUS = 1.0;
@@ -308,10 +308,19 @@ public:
 		skyTextureId = createSky(cracksDirectory, faces);
 	}
 
-	vec3 rockEquation(int z) {
-		double y = pow((0.16 * z), 4);
-		return vec3(0, y, GRID_SCALE*z);
-	}
+	//Rock generation constants
+	double LINE_SLOPE = -8.0;
+	float LINE_Y_OFFSET = 0;
+	float ROCK_OFFSET_MAX = 4.0;
+	float START_VALUE = -NUM_ROCKS*GRID_SCALE;
+	float LINE_Z_OFFSET = START_VALUE;
+	float COLLISION_PLANE_OFFSET = 12;
+	float BIRD_RADIUS = 3.5;
+	float WALL_HEIGHT = 4;
+	float WALL_WIDTH = 39;
+	int NUM_WALLS_WIDE = 5;
+
+
 
 	void addRock(vec3 position) {
 		shared_ptr<Entity> rock = make_shared<Entity>(position, vec3(1.0), vec3(0), false);
@@ -320,36 +329,59 @@ public:
 		rockPositions.push_back(position);
 	}
 
-	float ROCK_OFFSET_MAX = 4.0;
-
-	//Gets random number between 0 and ROCK_OFFSET_MAX
+	//Gets random number between -ROCK_OFFSET_MAX and ROCK_OFFSET_MAX
 	float randOffset() {
-		float numer =(rand() % 100);
+		float numer =(rand() % 100); 
 		return numer / (100.0/ROCK_OFFSET_MAX) - ROCK_OFFSET_MAX/2;
+	}
+
+	//in the Y-Z plane
+	vec3 rockEquation(int z, int x = 0) {
+		//double y = pow((0.16 * z), 4);
+		double y = LINE_SLOPE * (z + LINE_Z_OFFSET) + LINE_Y_OFFSET;
+		return vec3(x, y, z);
 	}
 
 	void init(const std::string& resourceDirectory)
 	{
-
+		// vec3,    vec3,   vec3,     bool
+		//position, scale, roation, moving
 		GLSL::checkVersion();
 
-		float birdZ = rockEquation(-NUM_ROCKS).y + GRID_SCALE;
-		bird = make_shared<Entity>(vec3(0, birdZ, 0), vec3(1.0), vec3(0), true);
-		bird->colliders.push_back(make_shared<SphereCollider>(bird->position, SPHERE_RADIUS));
+		vec3 rockStart = rockEquation(START_VALUE);
 
-		/*wall = make_shared<Entity>(vec3(0, 0, 15), vec3(60, 60, .05), vec3(1, 0, 0), false);
-		wall->colliders.push_back(make_shared<PlaneCollider>(vec3(-30, 0, 14), vec3(0, 60, 22), vec3(30, 0, 14)));
-		entities.push_back(wall);*/
+		bird = make_shared<Entity>(vec3(rockStart.x, rockStart.y+GRID_SCALE, rockStart.z+GRID_SCALE), vec3(1.0), vec3(0), true);
+		bird->colliders.push_back(make_shared<SphereCollider>(bird->position, BIRD_RADIUS));
 
-		for (int i = -NUM_ROCKS; i <= 0; i++) {
-			vec3 position1 = rockEquation(i) + vec3(0, randOffset(), NUM_ROCKS*GRID_SCALE);
-			addRock(position1);
+		vec3 wallCenter = rockEquation(START_VALUE)-vec3(0, GRID_SCALE, 0);
+		wall = make_shared<Entity>(wallCenter, vec3(.2), vec3(1, 0, 0), false);
+		wall->colliders.push_back(make_shared<PlaneCollider>(
+			vec3(-1, wallCenter.y + COLLISION_PLANE_OFFSET, wallCenter.z), 
+			vec3(1, wallCenter.y + COLLISION_PLANE_OFFSET, wallCenter.z), 
+			vec3(0, wallCenter.y-LINE_SLOPE + COLLISION_PLANE_OFFSET, wallCenter.z-1)));
+		entities.push_back(wall);
 
-			vec3 position2 = position1 + vec3(GRID_SCALE, randOffset(), 0);
-			addRock(position2);
+		//Starting rock
+		addRock(rockEquation(START_VALUE));
+
+		for (int i = START_VALUE+GRID_SCALE; i <= 0; i+=GRID_SCALE) {
+			//rock center
+			vec3 position1 = rockEquation(i) + vec3(0, randOffset(), 0);
+			if (rand() % 2 == 1) {
+				addRock(position1);
+			}
+
+			//rock right
+			if (rand() % 2 == 1) {
+				vec3 position2 = position1 + vec3(GRID_SCALE*2, randOffset(), 0);
+				addRock(position2);
+			}
 			
-			vec3 position3 = position1 + vec3(-GRID_SCALE, randOffset(), 0);
-			addRock(position3);
+			//rock left
+			if (rand() % 2 == 1) {
+				vec3 position3 = position1 + vec3(-GRID_SCALE*2, randOffset(), 0);
+				addRock(position3);
+			}
 
 		}
 
@@ -437,6 +469,17 @@ public:
 			meshsphere->createShape(TOshapesSphere[0]);
 			meshsphere->measure();
 			meshsphere->init();
+		}
+
+		rc = tinyobj::LoadObj(TOshapesWall, objMaterialsWall, errStr, (resourceDirectory + "/cliff_3.obj").c_str());
+		if (!rc) {
+			cerr << errStr << endl;
+		}
+		else {
+			meshwall = make_shared<Shape>();
+			meshwall->createShape(TOshapesWall[0]);
+			meshwall->measure();
+			meshwall->init();
 		}
 
 		rc = tinyobj::LoadObj(TOshapesPillar, objMaterialsPillar, errStr, (resourceDirectory + "/squareRock.obj").c_str());
@@ -539,15 +582,29 @@ public:
 	}
 
 	void drawWall(shared_ptr<MatrixStack> Model) {
-		
+		vec3 wallPos = wall->position;
+
 		SetMaterial(6);
-		Model->pushMatrix();
-		Model->translate(wall->position);
-		Model->rotate(PI*.1, wall->rotation);
-		Model->scale(wall->scale);
-		setModel(progMat, Model);
-		meshfloor->draw(progMat);
-		Model->popMatrix();
+
+		//Start with furthest left wall tile
+		//wallPos = wall->position - vec3(WALL_WIDTH*(NUM_WALLS_WIDE/2), 0, 0);
+
+		for (int curWallsWide = 0; curWallsWide < NUM_WALLS_WIDE; curWallsWide++) {
+			//Move wall right for next wall tile (this is still slightly buggy)
+			wallPos = wall->position - vec3(WALL_WIDTH*((NUM_WALLS_WIDE/2-curWallsWide) / 2), 0, 0);
+
+			for (int i = 0; i < NUM_ROCKS*2; i++) {
+				Model->pushMatrix();
+				Model->translate(wallPos);
+				Model->rotate(PI + PI / 2 - atan(LINE_SLOPE), wall->rotation);
+				Model->scale(wall->scale);
+				setModel(progMat, Model);
+				meshwall->draw(progMat);
+				Model->popMatrix();
+
+				wallPos += vec3(0, LINE_SLOPE*WALL_HEIGHT, WALL_HEIGHT);
+			}
+		}
 	}
 
 	void drawBird(shared_ptr<MatrixStack> Model) {
@@ -566,7 +623,7 @@ public:
 
 	void drawRocks(shared_ptr<MatrixStack> Model) {
 		for (int i = 0; i < rockPositions.size(); i++) {
-			SetMaterial(7);
+			SetMaterial(2);
 			Model->pushMatrix();
 			Model->translate(rockPositions[i]);
 			//Model->scale(vec3(1.25, .5, 1.25)); pillar scaling
@@ -633,7 +690,7 @@ public:
 
 		// Apply perspective projection.
 		Projection->pushMatrix();
-		Projection->perspective(45.0f, aspect, 0.01f, 100.0f);
+		Projection->perspective(45.0f, aspect, 0.01f, 1000.0f);
 
 		// Draw a stack of cubes with indiviudal transforms
 		
@@ -647,7 +704,7 @@ public:
 				Model->rotate(rotate, vec3(0, 1, 0));
 				drawFloor(Model);
 				drawBird(Model);
-				//drawWall(Model);
+				drawWall(Model);
 				drawRocks(Model);
 				//drawGeeseBlue(Model);
 			Model->popMatrix();
