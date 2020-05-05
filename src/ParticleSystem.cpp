9c621f47d6b748d4b1d6731e5ae4880d751150c0
Chanelle <ckaith97@yahoo.com>
2020-05-05 16:38:23 -0700

    temp commit

--- src/ParticleSystem.cpp
+++ src/ParticleSystem.cpp
@@ -1,1 +1,16 @@
 #include "ParticleSystem.h"
+
+void ParticleSystem::update(float delta_frame) {
+
+	prog->bind();
+	glBindVertexArray(VAO);
+	glEnableVertexAttribArray(0);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glDepthMask(false);
+
+	for (Particle particle : particles) {
+
+	}
+
+}
