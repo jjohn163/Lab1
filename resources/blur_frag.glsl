2bbcb125b69745f88922ff10a5a0e381cb3088cb
Kevin Krein <kkrein@calpoly.edu>
2020-06-03 16:36:50 -0700

    Getting started

--- resources/blur_frag.glsl
+++ resources/blur_frag.glsl
@@ -11,7 +11,23 @@ uniform float weight[5] = float[]( 0.2270270270, 0.1945945946, 0.1216216216, 0.0
 
 uniform float unblurredRadius;
 
+uniform mat3 G[9] = mat3[](
+	1.0/(2.0*sqrt(2.0)) * mat3( 1.0, sqrt(2.0), 1.0, 0.0, 0.0, 0.0, -1.0, -sqrt(2.0), -1.0 ),
+	1.0/(2.0*sqrt(2.0)) * mat3( 1.0, 0.0, -1.0, sqrt(2.0), 0.0, -sqrt(2.0), 1.0, 0.0, -1.0 ),
+	1.0/(2.0*sqrt(2.0)) * mat3( 0.0, -1.0, sqrt(2.0), 1.0, 0.0, -1.0, -sqrt(2.0), 1.0, 0.0 ),
+	1.0/(2.0*sqrt(2.0)) * mat3( sqrt(2.0), -1.0, 0.0, -1.0, 0.0, 1.0, 0.0, 1.0, -sqrt(2.0) ),
+	1.0/2.0 * mat3( 0.0, 1.0, 0.0, -1.0, 0.0, -1.0, 0.0, 1.0, 0.0 ),
+	1.0/2.0 * mat3( -1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, -1.0 ),
+	1.0/6.0 * mat3( 1.0, -2.0, 1.0, -2.0, 4.0, -2.0, 1.0, -2.0, 1.0 ),
+	1.0/6.0 * mat3( -2.0, 1.0, -2.0, 1.0, 4.0, 1.0, -2.0, 1.0, -2.0 ),
+	1.0/3.0 * mat3( 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 )
+);
+
 void main(){
+	mat3 I;
+	float cnv[9];
+	vec3 sample;
+
 	vec3 texColor = texture( texBuf, texCoord ).rgb;	
 
 	float unblurred = abs(unblurredRadius);
@@ -27,4 +43,23 @@ void main(){
 		color += vec4(texture( texBuf, texCoord + vec2(offset[i], 0.0)/512.0 ).rgb, 1) * weight[i] * middleness;
 		color += vec4(texture( texBuf, texCoord - vec2(offset[i], 0.0)/512.0 ).rgb, 1) * weight[i] * middleness;
 	}
+
+	
+	/* fetch the 3x3 neighbourhood and use the RGB vector's length as intensity value */
+	for (int i=0; i<3; i++)
+	for (int j=0; j<3; j++) {
+		sample = texelFetch( texBuf, ivec2(gl_FragCoord) + ivec2(i-1,j-1), 0 ).rgb;
+		I[i][j] = length(sample); 
+	}
+	
+	/* calculate the convolution values for all the masks */
+	for (int i=0; i<9; i++) {
+		float dp3 = dot(G[i][0], I[0]) + dot(G[i][1], I[1]) + dot(G[i][2], I[2]);
+		cnv[i] = dp3 * dp3; 
+	}
+
+	float M = (cnv[0] + cnv[1]) + (cnv[2] + cnv[3]);
+	float S = (cnv[4] + cnv[5]) + (cnv[6] + cnv[7]) + (cnv[8] + M); 
+	
+	color = color+.8*vec4(sqrt(M/S));
 }
